eaebdd2bc6d1131dcb3515b5ecf10893380ec9bc
sangbeom ma <tonystark@261house.com>
2019-03-30 16:06:07 +0900

    Init Server Application -2-

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setViewEngine('hbs');
+  const options = new DocumentBuilder()
+    .setTitle('PNU-ILRRS')
+    .setDescription('API Application')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
